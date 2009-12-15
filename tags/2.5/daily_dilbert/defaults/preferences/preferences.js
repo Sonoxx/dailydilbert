3e30c75056716f31c841fc1fef02a2ec61236427
gouvTarkin <gouvTarkin@75c1cc9f-5249-0410-a817-c1b279ab80ea>
2009-12-15 11:37:24 +0000

    Fixes from Community

---
+++ tags/2.5/daily_dilbert/defaults/preferences/preferences.js
@@ -0,0 +1,27 @@
+pref('extensions.daily_dilbert.popup.width.max',1024);
+pref('extensions.daily_dilbert.popup.height.max',768);
+pref('extensions.daily_dilbert.comic.site.current',0);
+pref('extensions.daily_dilbert.comic.site.count',19);
+pref('extensions.{C598822D-6E25-4ADB-9137-D52C050F315C}.description','chrome://daily_dilbert/locale/daily_dilbert.properties');
+pref('extensions.daily_dilbert.comic.site.0','Dilbert[en]$$http://$$feeds.feedburner.com/DilbertDailyStrip?format=xml$$dilbert.com/dyn/str_strip[\\d\\/]+.strip.print.gif');
+pref('extensions.daily_dilbert.comic.site.1','Dilbert[es]$$http://www.unitedmedia.com/$$spanish/dilbert/archive/index.html$$spanish[\\w\\/\\.]*dilbert\\d+\\.(png|PNG|jpg|JPG|gif|GIF|tif|TIF)');
+pref('extensions.daily_dilbert.comic.site.2','Dilbert[cz]$$http://$$ekonomika.idnes.cz/dilbert.asp$$i\\.idnes\\.cz[\\w\\/\\.]+maxi[\\w\\/\\.]+_?dilb?[\\w_]*\\.(png|PNG|jpg|JPG|gif|GIF|tif|TIF)');
+pref('extensions.daily_dilbert.comic.site.3','Dilbert[de]$$http://www.vdi-nachrichten.com/$$allgemein/dilbertshow.asp$$_library/dilbert/\\w+_\\w+\\.(png|PNG|jpg|JPG|gif|GIF|tif|TIF)');
+pref('extensions.daily_dilbert.comic.site.4','Dilbert[pl]$$http://$$praca.gazeta.pl/gazetapraca/0,74783.html$$bi\\.gazeta\\.pl[\\w\\/]*\\.(png|PNG)');
+pref('extensions.daily_dilbert.comic.site.5','Dilbert[se]$$http://tjanster.idg.se/dilbertimages/dil<YYYY><MM><DD>.gif');
+pref('extensions.daily_dilbert.comic.site.6','Andy Capp[en]$$http://images.ucomics.com/comics/crcap/<YYYY>/crcap<YY><MM><DD>.gif');
+pref('extensions.daily_dilbert.comic.site.7','Bloom County[en]$$http://images.ucomics.com/comics/blm/<YYYY>/blm<YY><MM><DD>.gif');
+pref('extensions.daily_dilbert.comic.site.8','Foxtrot[en]$$http://images.ucomics.com/comics/ft/<YYYY>/ft<YY><MM><DD>.gif');
+pref('extensions.daily_dilbert.comic.site.9','Garfield[en]$$http://images.ucomics.com/comics/ga/<YYYY>/ga<YY><MM><DD>.gif');
+pref('extensions.daily_dilbert.comic.site.10','Get Fuzzy[en]$$http://$$comics.com/get_fuzzy/$$[\d\].cdn.cloudfiles.rackspacecloud.com/dyn/str_strip[\d\/]+.full.gif');
+pref('extensions.daily_dilbert.comic.site.11','Liberty Meadows[en]$$http://$$comics.com/liberty_meadows/$$[\d\].cdn.cloudfiles.rackspacecloud.com/dyn/str_strip[\d\/]+.full.gif');
+pref('extensions.daily_dilbert.comic.site.12','Non Sequitur[en]$$http://images.ucomics.com/comics/nq/<YYYY>/nq<YY><MM><DD>.gif');
+pref('extensions.daily_dilbert.comic.site.13','Over the Hedge[en]$$http://$$comics.com/over_the_hedge/$$[\d\].cdn.cloudfiles.rackspacecloud.com/dyn/str_strip[\d\/]+.full.gif');
+pref('extensions.daily_dilbert.comic.site.14','Peanuts[en]$$http://$$comics.com/peanuts/$$[\d\].cdn.cloudfiles.rackspacecloud.com/dyn/str_strip[\d\/]+.full.gif');
+pref('extensions.daily_dilbert.comic.site.15','Pearls[en]$$http://$$comics.com/pearls_before_swine/$$[\d\].cdn.cloudfiles.rackspacecloud.com/dyn/str_strip[\d\/]+.full.gif');
+pref('extensions.daily_dilbert.comic.site.16','PhD Comics[en]$$http://www.phdcomics.com/comics/$$archive.php$$archive/phd\d+s\.(png|PNG|jpg|JPG|gif|GIF|tif|TIF)');
+pref('extensions.daily_dilbert.comic.site.17','User Friendly[en]$$http://$$ars.userfriendly.org/cartoons/$$www.userfriendly.org/cartoons/archives/[\\w\\/\\.]*\\.gif');
+pref('extensions.daily_dilbert.comic.site.18','xkcd[en]$$http://$$www.xkcd.org/index.html$$imgs.xkcd.com/comics/[\w_]*.png');
+
+
+
